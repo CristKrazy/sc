17933310bb0bbbd404965e2aac178c6d280ff48e
ChrisMaster-Krazy <128928635+CristKrazy@users.noreply.github.com>
2023-05-12 18:44:59 +0700

    Update sc.py

--- sc.py
+++ sc.py
@@ -6,7 +6,7 @@ import sys
 import socket
 from datetime import datetime
 
-ascii_banner = pyfiglet.figlet_format("CM-KRAZY")
+ascii_banner = pyfiglet.figlet_format("SCAN PORT")
 print(ascii_banner)
 
 target = input(str("[-] IP: "))
